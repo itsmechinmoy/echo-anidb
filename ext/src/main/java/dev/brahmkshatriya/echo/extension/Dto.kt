1c88462f5942964de4e8412e8af6da3a422954b8
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-22 17:32:02 +0530

    [skip ci]

--- ext/src/main/java/dev/brahmkshatriya/echo/extension/Dto.kt
+++ ext/src/main/java/dev/brahmkshatriya/echo/extension/Dto.kt
@@ -16,18 +16,21 @@ data class EpisodeDto(
     val number2: Double? = null,
     val filler: Boolean = false,
 ) {
-    fun getFormattedTitle(offset: Float, showFillerTag: Boolean = true): String {
+    fun getEpisodeLabel(offset: Float): String {
         val num = number.toFloat()
         val num2 = number2?.toFloat()
         val adjustedNumber = num - offset
         val adjustedNumber2 = num2?.let { it - offset }
 
-        val label = if (adjustedNumber2 != null && adjustedNumber2 != 0f && adjustedNumber2 != adjustedNumber) {
+        return if (adjustedNumber2 != null && adjustedNumber2 != 0f && adjustedNumber2 != adjustedNumber) {
             "${adjustedNumber.toString().removeSuffix(".0")}\u2013${adjustedNumber2.toString().removeSuffix(".0")}"
         } else {
             adjustedNumber.toString().removeSuffix(".0")
         }
+    }
 
+    fun getFormattedTitle(offset: Float, showFillerTag: Boolean = true): String {
+        val label = getEpisodeLabel(offset)
         var name = "Episode $label"
         if (filler && showFillerTag) name += " (Filler)"
         return name
